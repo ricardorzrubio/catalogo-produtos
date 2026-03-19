4cece42c7b5caa5b7056256fdead8ac037cb5bb1
Ricardo Ruiz Rubio <ricardo.rz.rubio@gmail.com>
2026-03-19 20:49:12 -0300

    Sprint 1 - setup inicial

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ padding: "20px", background: "#222", color: "#fff" }}>
+      <h2>Minha Loja</h2>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+function ProductCard({ produto }) {
+    return (
+        <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+            <img src={produto.image} width="100" />
+            <h3>{produto.title}</h3>
+            <p>
+                {produto.price.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                })}
+            </p>
+        </div>
+    );
+}
+
+export default ProductCard;
