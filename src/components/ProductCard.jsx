6ed4c9aae6fa18951cc72c10bba1741682f5c1ec
Ricardo Ruiz Rubio <ricardo.rz.rubio@gmail.com>
2026-03-19 21:09:10 -0300

    melhorias no layout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,20 @@
+import "./ProductCard.css";
+
 function ProductCard({ produto }) {
-    return (
-        <div style={{ border: "1px solid #ccc", padding: "10px" }}>
-            <img src={produto.image} width="100" />
-            <h3>{produto.title}</h3>
-            <p>
-                {produto.price.toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                })}
-            </p>
-        </div>
-    );
+  return (
+    <div className="card">
+      <img src={produto.image} alt={produto.title} />
+
+      <h3>{produto.title}</h3>
+
+      <p className="price">
+        {produto.price.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </p>
+    </div>
+  );
 }
 
 export default ProductCard;
